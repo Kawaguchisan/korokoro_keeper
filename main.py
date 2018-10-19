ef145f9dd5d476be54cc7678015c0ec24847587e
Kawaguchisan <m183305w@st.u-gakugei.ac.jp>
2018-10-19 19:02:43 +0900

    Completed

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from sklearn.linear_model import LinearRegression
 import random
 
 # consts ------------------------------------------------------
-
-STAGE_LENGTH = 100
-GOAL_NUMBER = 10
+MIN_ANGLE = 180
+MAX_ANGLE = 210
 
 TEST_DATA_CSV_FILE_NAME = "./test_data.csv"
 FUNCTION_DATA_CSV_FILE_NAME = "./function_data.csv"
@@ -112,7 +111,7 @@ def get_goal_button_condition():
 
 def get_angle():
   angle = ANGLE_SENSOR_MCP3008_INPUT.value
-  return round(angle, 5)
+  return round(angle, 5) * 360
 
 def get_goal_location():
   start = time.time()
@@ -164,14 +163,22 @@ def led_default_light_up():
     GPIO.output(ORANGE_LED_GPIO_OUTPUT, True)
 
 def fire_a_ball(move):
-   if (move):
-      # move downward
-      GPIO.output(FIRING_MOTOR_GPIO_OUTPUT1, False)
-      GPIO.output(FIRING_MOTOR_GPIO_OUTPUT2, True)
-      time.sleep(0.4)
+  if (move):
+    # move downward
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT1, False)
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT2, True)
+    time.sleep(0.4)
 
-      GPIO.output(FIRING_MOTOR_GPIO_OUTPUT1, False)
-      GPIO.output(FIRING_MOTOR_GPIO_OUTPUT2, False)
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT1, False)
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT2, False)
+  else:
+    # move upward
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT1, True)
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT2, False)
+    time.sleep(0.4)
+
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT1, False)
+    GPIO.output(FIRING_MOTOR_GPIO_OUTPUT2, False)
 
 def move_a_keeper(moving_time):
   if moving_time > 0:
@@ -187,15 +194,28 @@ def move_a_keeper(moving_time):
     GPIO.output(KEEPER_MOTOR_GPIO_OUTPUT1, False)
     GPIO.output(KEEPER_MOTOR_GPIO_OUTPUT2, False)
 
+def get_prediction_goal_location(angle):
+  # Read function data
+  dataset = pd.read_csv(FUNCTION_DATA_CSV_FILE_NAME)
+  coefficient = dataset['coefficient'].values[0]
+  intercept = dataset['intercept'].values[0]
+  
+  # Predict goal_location
+  goal_location = angle * coefficient + intercept
+  return goal_location
+
+def calculate_moving_time(goal_location):
+  # Calculate goal_location
+  moving_time = goal_location - 1     # (1 to 10 => 0 to 9)
+  moving_time = moving_time / 9 * 2.6 # (0 to 9  => 0 to 2.6)
+  moving_time = moving_time - 1.3     # (0 to 2.6 => -1.3 to 1.3)
+  return moving_time
+  
 def write_function_csv_file():
   # Create dataset and including the first row by setting no header as input
   dataset = pd.read_csv(TEST_DATA_CSV_FILE_NAME)
   x = dataset[[COLUMNS[0]]].values
   y = dataset[COLUMNS[1]].values
-
-  # Shape the goal location
-  goal_length = STAGE_LENGTH / GOAL_NUMBER
-  y = [goal_length * element + goal_length / 2.0 for element in y]
 
   # Learn the weight of linear model
   linear_regression = LinearRegression()
@@ -221,8 +241,6 @@ while True:
   try:
     change_function_button_condition = get_change_function_button_condition()
     ball_firing_button_condition = get_ball_firing_button_condition()
-    print('ball_firing_button_condition: ' + str(ball_firing_button_condition))
-    print('change_function_button_condition: ' + str(change_function_button_condition))
 
     if change_function_button_condition == 1:
       change_function_condition()
@@ -231,61 +249,32 @@ while True:
 
     # training data utilization phase
     if function_condition == 1:
-      print("training data utilization phase")
       if ball_firing_button_condition == 1:
-        print("ball firing !")
-
         # light up red LED
 	GPIO.output(RED_LED_GPIO_OUTPUT, True)
 
 	# get angle
         angle = get_angle()
-	print('angle:' + str(angle))
+
+        # validate angle
+        if (not(angle >= MIN_ANGLE and angle <= MAX_ANGLE)):
+          # light down red LED
+    	  GPIO.output(RED_LED_GPIO_OUTPUT, False)
+          print ("invalidate angle")
+          continue
+
+	# move keeper (operation moter) : exactly
+	prediction_goal_location = get_prediction_goal_location(angle)
+        print('prediction_goal_location: ' + str(prediction_goal_location) + '\n')
+
+	# calculate moving_time
+	moving_time = calculate_moving_time(prediction_goal_location)
+        print('moving_time: ' + str(moving_time) + '\n')
+        move_a_keeper(moving_time)
+        time.sleep(0.5)
 
 	# fire a ball (operation moter)
-        fire_a_ball()
-
-	# ------------------------------------------------------------------------------ TODO -----------------------------------------------------------------------------------
-	# move keeper (operation moter) : exactly
-	# ------------------------------------------------------------------------------ TODO -----------------------------------------------------------------------------------
-
-        # light down red LED
-    	GPIO.output(RED_LED_GPIO_OUTPUT, False)
-
-    # training data collection phase
-    else:
-      print("training data collection phase")
-
-      if ball_firing_button_condition == 1:
-        print("ball firing !")
-
-        # light up red LED
-    	GPIO.output(RED_LED_GPIO_OUTPUT, True)
-
-	# get angle
-	angle = get_angle()
-	print('angle:' + str(angle))
-
-        # fire a ball (operation moter)
         fire_a_ball(True)
-
-	# ------------------------------------------------------------------------------ TODO -----------------------------------------------------------------------------------
-	# move keeper (operation moter) : rondomly
-	moving_time = random.uniform(1.3, -1.3)
-        move_a_keeper(moving_time)
-
-        # get goal_location
-	goal_location = get_goal_location()
-        print('goal_location: ' + goal_location)
-
-        # write Test CSV file
-	data = pd.DataFrame([[angle, goal_location]])
-	data.to_csv(
-          TEST_DATA_CSV_FILE_NAME, index=False, encoding='utf-8', mode='a', header=False
-	)
-
-        # calculate and write Function CSV file
-	# write_function_csv_file()
 
         # fire a ball (operation moter) : return to	
         fire_a_ball(False)
@@ -294,13 +283,64 @@ while True:
 
         # light down red LED
     	GPIO.output(RED_LED_GPIO_OUTPUT, False)
+
+    # training data collection phase
+    else:
+      if ball_firing_button_condition == 1:
+
+        # light up red LED
+    	GPIO.output(RED_LED_GPIO_OUTPUT, True)
+
+	# get angle
+	angle = get_angle()
+        
+        # validate angle
+        if (not(angle >= MIN_ANGLE and angle <= MAX_ANGLE)):
+          # light down red LED
+    	  GPIO.output(RED_LED_GPIO_OUTPUT, False)
+          print ("invalidate angle")
+          continue
+
+	# move keeper (operation moter) : rondomly
+	moving_time = random.uniform(1.3, -1.3)
+        move_a_keeper(moving_time)
+        time.sleep(0.5)
+
+        # fire a ball (operation moter)
+        fire_a_ball(True)
+
+	# ------------------------------------------------------------------------------ TODO -----------------------------------------------------------------------------------
+        # get goal_location
+  	print("start get goal location!")
+	goal_location = get_goal_location()
+	print('angle: ' + str(angle) + ', goal_location: ' + str(goal_location))
+  	print("end get goal location!\n")
+
+	if (goal_location != 'None'):
+
+          # write Test CSV file
+  	  data = pd.DataFrame([[angle, goal_location]])
+	  data.to_csv(
+            TEST_DATA_CSV_FILE_NAME, index=False, encoding='utf-8', mode='a', header=False
+	  )
+
+          # calculate and write Function CSV file
+	  write_function_csv_file()
+
 	# ------------------------------------------------------------------------------ TODO -----------------------------------------------------------------------------------
 
-    time.sleep(0.5)
+        # fire a ball (operation moter) : return to	
+        fire_a_ball(False)
+	# move keeper (operation moter) : return to
+        move_a_keeper(-moving_time)
+
+        # light down red LED
+    	GPIO.output(RED_LED_GPIO_OUTPUT, False)
+
+    time.sleep(0.15)
 
   except IOError:
     print('Error !')
 
 GPIO.cleanup()
 
-# -------------------------------------------------------------
---
+++ watch_data.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+while :
+do
+  echo "---------- cat test_data.csv ----------"
+  cat ./test_data.csv
+  echo "---------- cat function_data.csv ----------"
+  cat ./function_data.csv
+  sleep 2
+  echo "\n"
+done
